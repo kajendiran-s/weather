cfc0e9ae015e058e31584da3fc15a46bb09f8f12
Kajendiran <skaje29062003@gmail.com>
2023-08-27 16:46:54 +0530

    Weather commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React,{useState,useContext, useEffect} from "react";
 import './App.css';
+import Weather from "./components/Weather";
+import {Dimmer, Loader} from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+  const [lat,setLat] = useState([]);
+  const [long,setLong] = useState([]);
+  const [data,setData] = useState([])
+  
+  useEffect(()=>{
+    const fetchData= async ()=>{
+    navigator.geolocation.getCurrentPosition(
+      function(position){
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+        console.log("Latitue is:",lat)
+        console.log("Longtit is:",long)
+      })
+
+      
+    await fetch(`${process.env.REACT_APP_API_URL}/weather?lat=${lat}&lon=${long}&units=metric&appid=${process.env.REACT_APP_API_KEY}`)
+    .then(res=>res.json())
+    .then(result=>{
+      setData(result)
+      console.log(result)
+    });
+  }
+  fetchData();}
+  ,[lat,long])
+
+  return(<div className="App">
+    {
+      (typeof data.main != 'undefined')?
+      (<Weather weatherData = {data}/>):
+      (<div>
+        <Dimmer active>
+          <Loader>Loading...</Loader>
+        </Dimmer>
+      </div>)
+    }
+  </div>);
 }
-
-export default App;
